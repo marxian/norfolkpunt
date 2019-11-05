ef403335b19e0ad96f76100e035847fdf4e995c9
Rupert Redington <rupert@neontribe.co.uk>
2019-11-05 15:35:30 +0000

    feat: link to punt page

--- components/PuntCard.js
+++ components/PuntCard.js
@@ -1,29 +1,30 @@
 import React from 'react'
+import Link from 'next/link'
 import images from '../images'
 import Image from './Image'
 
 export default ({ punt, className }) => (
   <div className={`${className} bg-black-90 white tc grow`}>
-    {/* <Link href={`/boats/${punt.slug}`}>
-      <a className="db link dim tc"> */}
-    {punt.coverImage ? (
-      <Image
-        data={{
-          ...images[punt.coverImage],
-          aspectRatio: 1,
-        }}
-      />
-    ) : (
-      <Image
-        data={{
-          lqip: require('../images/photograph-wanted.png?lqip'),
-          fluid: require('../images/photograph-wanted.png?resize&sizes[]=200&sizes[]=600&sizes[]=1000&sizes=2000'),
-        }}
-      />
-    )}
-    <span className="db f4 b pt1">{punt.name}</span>
-    <span className="db f5 light-gray b pb1">{punt.sailNumber}</span>
-    {/* </a>
-    </Link> */}
+    <Link href={`/boats/${punt.slug}`}>
+      <a className="db link dim tc">
+        {punt.coverImage ? (
+          <Image
+            data={{
+              ...images[punt.coverImage],
+              aspectRatio: 1,
+            }}
+          />
+        ) : (
+          <Image
+            data={{
+              lqip: require('../images/photograph-wanted.png?lqip'),
+              fluid: require('../images/photograph-wanted.png?resize&sizes[]=200&sizes[]=600&sizes[]=1000&sizes=2000'),
+            }}
+          />
+        )}
+        <span className="db f4 white b pt1">{punt.name}</span>
+        <span className="db f5 light-gray b pb1">{punt.sailNumber}</span>
+      </a>
+    </Link>
   </div>
 )
