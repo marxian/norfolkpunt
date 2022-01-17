622d604a4091b73c462ebf4bbcb16e56078592e2
Rupert Redington <rupert@lola.tech>
2022-01-17 12:53:55 +0000

    feat: csp

--- next.config.js
+++ next.config.js
@@ -1,8 +1,40 @@
 const { withPlaiceholder } = require('@plaiceholder/next')
 const { withPlausibleProxy } = require('next-plausible')
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; style-src 'unsafe-inline'; script-src 'self' 'unsafe-eval';",
+  },
+]
+
 const plaiceholder = withPlaiceholder({
-  // your Next.js config
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 })
 
 module.exports = withPlausibleProxy(plaiceholder)
