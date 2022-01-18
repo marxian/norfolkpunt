d3b6da4b52c4f811bf8db81c91b1ff2704e997df
Rupert Redington <rupert@lola.tech>
2022-01-18 19:21:31 +0000

    Update index.js

--- pages/boats/index.js
+++ pages/boats/index.js
@@ -11,7 +11,7 @@ const Boats = ({ punts }) => {
   )
 
   return (
-    <SimpleGrid m="20px" columns={[1, 2, 3, 4]} spacing="20px">
+    <SimpleGrid m="20px" columns={[1, 2, 3]} spacing="20px">
       {boats.map(({ data, slug, image }) => (
         <PuntCard key={slug} punt={data} slug={slug} image={image} />
       ))}
