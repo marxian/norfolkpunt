99cd9371bbe72df0feb9faae566ac134c828c0f1
Rupert Redington <rupert@lola.tech>
2022-01-24 17:45:22 +0000

    feat: captions and attributions for gallery images

--- pages/gallery.js
+++ pages/gallery.js
@@ -18,12 +18,12 @@ const Gallery = ({ images }) => {
         w="100%"
         maxW="1200px"
         mx="auto"
-        sx={{ columnCount: [1, 2, 3, 4], columnGap: '8px' }}
+        sx={{ columnCount: [1, 2, 3, 4], columnGap: '10px' }}
       >
         {images.map((pic, i) => (
           <Link
-            href={pic.img.src}
-            key={`${pic.url}${i}`}
+            href={pic.src}
+            key={`${pic.src}${i}`}
             onClick={(e) => {
               e.preventDefault()
               setPhotoIndex(images.indexOf(pic))
@@ -33,7 +33,7 @@ const Gallery = ({ images }) => {
             <Image
               {...pic.img}
               blurDataURL={pic.base64}
-              alt={pic.caption || ''}
+              alt={pic.caption}
               placeholder="blur"
               borderRadius="xl"
             />
@@ -42,10 +42,10 @@ const Gallery = ({ images }) => {
       </Box>
       {isOpen && (
         <Lightbox
-          mainSrc={images[photoIndex]?.img.src}
-          nextSrc={images[(photoIndex + 1) % images.length]?.img.src}
+          mainSrc={images[photoIndex]?.src}
+          nextSrc={images[(photoIndex + 1) % images.length]?.src}
           prevSrc={
-            images[(photoIndex + images.length - 1) % images.length]?.img.src
+            images[(photoIndex + images.length - 1) % images.length]?.src
           }
           onCloseRequest={() => setOpen(false)}
           onMovePrevRequest={() =>
@@ -54,6 +54,8 @@ const Gallery = ({ images }) => {
           onMoveNextRequest={() =>
             setPhotoIndex((photoIndex + 1) % images.length)
           }
+          imageCaption={<h2>&copy; {images[photoIndex]?.attribution}</h2>}
+          imageTitle={<h3>{images[photoIndex]?.caption}</h3>}
         />
       )}
     </>
@@ -65,7 +67,10 @@ export async function getStaticProps({ params }) {
 
   const images = []
   for (const pic of page.data.images) {
-    let image = await getPlaiceholder(pic)
+    let image = {
+      ...pic,
+      ...(await getPlaiceholder(pic.src)),
+    }
     images.push(image)
   }
 
