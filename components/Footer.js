9d2be5826f2f37f74f5b18b03b458bbcba4df27e
Rupert Redington <rupert@lola.tech>
2022-03-13 16:44:59 +0000

    feat: add log in link

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Box, Flex, HStack, Link, Text } from '@chakra-ui/react'
+import { Box, Flex, Wrap, WrapItem, Link, Text } from '@chakra-ui/react'
 
 const Footer = () => (
   <Box color="white" bg={'black'} px={4}>
     <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-      <Link href={'mailto:norfolkpunt@gmail.com'} color={'white'}>
-        Contact
-      </Link>
+      <Wrap>
+        <WrapItem>
+          <Link href={'mailto:norfolkpunt@gmail.com'} color={'white'}>
+            Contact Us
+          </Link>
+        </WrapItem>
+        <WrapItem>
+          <Link href={'/admin.html'} color={'white'}>
+            Log In
+          </Link>
+        </WrapItem>
+      </Wrap>
+
       <Text>
         &copy;{new Date().getFullYear()} Norfolk&nbsp;Punt
         Owners&nbsp;Association
