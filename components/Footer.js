a3a72dd94eb355a814436ed9aec024543b554f52
Rupert Redington <rupert@lola.tech>
2022-03-19 09:49:49 +0000

    fix: responsive navbars

--- components/Footer.js
+++ components/Footer.js
@@ -1,34 +1,24 @@
 import React from 'react'
-import { Box, Flex, Wrap, WrapItem, Link, Text } from '@chakra-ui/react'
+import { Link, Text, SimpleGrid } from '@chakra-ui/react'
 
 const Footer = () => (
-  <Box color="white" bg={'black'} px={4}>
-    <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-      <Wrap>
-        <WrapItem>
-          <Link href={'mailto:norfolkpunt@gmail.com'} color={'white'}>
-            Contact Us
-          </Link>
-        </WrapItem>
-        <WrapItem>
-          <Link href={'/admin.html'} color={'white'} target="_blank">
-            Log In
-          </Link>
-        </WrapItem>
-      </Wrap>
-
-      <Text>
-        &copy;{new Date().getFullYear()} Norfolk&nbsp;Punt
-        Owners&nbsp;Association
-      </Text>
-      <Text>
-        Most photographs by kind permission &copy;&nbsp;
-        <Link href="https://www.myerscoughpictures.org.uk">
-          Robin&nbsp;Myerscough
-        </Link>
-      </Text>
-    </Flex>
-  </Box>
+  <SimpleGrid bg="black" color="white" columns={[1, 2, 3, 4]}>
+    <Link href={'mailto:norfolkpunt@gmail.com'} color={'white'}>
+      Contact Us
+    </Link>
+    <Link href={'/admin.html'} color={'white'} target="_blank">
+      Log In
+    </Link>
+    <Text>
+      &copy;{new Date().getFullYear()} Norfolk&nbsp;Punt Owners&nbsp;Association
+    </Text>
+    <Text>
+      Most photographs by kind permission &copy;&nbsp;
+      <Link href="https://www.myerscoughpictures.org.uk">
+        Robin&nbsp;Myerscough
+      </Link>
+    </Text>
+  </SimpleGrid>
 )
 
 export default Footer
